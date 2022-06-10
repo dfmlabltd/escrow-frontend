248c55efaf4dcbadc4a308a6a42c731cc906782b
iamoracle <officialbilas@gmail.com>
2022-06-10 21:03:08 +0100

    :art: restructured files
    restructured files

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -9,6 +9,7 @@
  */
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./ContractManager.sol";
 
 pragma solidity ^0.8.4;
 
@@ -37,7 +38,7 @@ contract Contract {
         address wallet;
         string description; // stored on IPFS
         uint256 points; // total approval points
-        WithdrawalRequestVote[] votes;
+        WithdrawalRequestVote [] votes;
         WithdrawalStatus status;
     }
 
@@ -219,10 +220,7 @@ contract Contract {
         uint256 remaingPoint = _request.amount - _request.points;
 
         if (balance >= remaingPoint) {
-            WithdrawalRequestVote memory vote = WithdrawalRequestVote(
-                msg.sender,
-                remaingPoint
-            );
+            WithdrawalRequestVote memory vote = WithdrawalRequestVote(msg.sender, remaingPoint);
             _request.points += remaingPoint;
             _request.votes.push(vote);
             _request.status = WithdrawalStatus.APPROVED;
@@ -260,73 +258,4 @@ contract Contract {
     }
 
     /************************************************************** FUNCTIONS END *************************************************************************** */
-}
-
-contract ContractManager {
-    event Deposit(address, address, uint256);
-    event Request(address, address, uint256, string);
-    event Rejected(address, address, address, string);
-    event Vote(address, address, address, string, uint256);
-    event Approval(address, address, address, string, uint256);
-
-    mapping(address => bool) _contracts;
-
-    constructor() {}
-
-    function deposit(
-        address contractAddress,
-        address depositor,
-        uint256 amount
-    ) external isValidContract {
-        emit Deposit(contractAddress, depositor, amount);
-    }
-
-    function request(
-        address contractAddress,
-        address depositor,
-        uint256 amount,
-        string memory description
-    ) external isValidContract {
-        emit Request(contractAddress, depositor, amount, description);
-    }
-
-    function reject(
-        address contractAddress,
-        address depositor,
-        address trustee,
-        string memory description
-    ) external isValidContract {
-        emit Rejected(contractAddress, depositor, trustee, description);
-    }
-
-    function vote(
-        address contractAddress,
-        address depositor,
-        address trustee,
-        string memory description,
-        uint256 balance
-    ) external isValidContract {
-        emit Vote(contractAddress, depositor, trustee, description, balance);
-    }
-
-    function approval(
-        address contractAddress,
-        address depositor,
-        address trustee,
-        string memory description,
-        uint256 balance
-    ) external isValidContract {
-        emit Approval(
-            contractAddress,
-            depositor,
-            trustee,
-            description,
-            balance
-        );
-    }
-
-    modifier isValidContract() {
-        require(_contracts[msg.sender], "UNAUTHORIZED");
-        _;
-    }
 }
