fbb9607b3457633f805819ab0330a5d1d6d66dbd
iamoracle <38940247+iamoracle@users.noreply.github.com>
2022-10-20 10:22:08 +0100

    ✨ added refresh token from endpoint

--- adehun/utils/helpers.ts
+++ adehun/utils/helpers.ts
@@ -1,4 +1,9 @@
-import { AUTH_ACCESS_TOKEN, AUTH_REFRESH_TOKEN } from "./constants";
+import axios from "axios";
+import {
+  AUTH_ACCESS_TOKEN,
+  AUTH_REFRESH_TOKEN,
+  REFRESH_TOKEN_ENDPOINT,
+} from "./constants";
 
 export function getAccessToken(): string | null {
   return localStorage.getItem(AUTH_ACCESS_TOKEN);
@@ -14,4 +19,12 @@ export function setAccessToken(token: string): void {
 
 export function setRefreshToken(token: string): void {
   localStorage.setItem(AUTH_REFRESH_TOKEN, token);
+}
+
+export async function refreshAccessToken(): Promise<string> {
+  const { data } = await axios.post(REFRESH_TOKEN_ENDPOINT, {
+    refresh: getRefreshToken(),
+  });
+  setAccessToken(data.access_token);
+  return data.access_token
 }
