a3169dbfbac9b6e8d4965f5c9d0eb654a142f67d
iamoracle <38940247+iamoracle@users.noreply.github.com>
2022-10-20 10:20:17 +0100

    🎨 simplified token hook

--- adehun/hooks/authentication/token.ts
+++ adehun/hooks/authentication/token.ts
@@ -1,51 +1,24 @@
-import axios from "axios";
-import exp from "constants";
 import { useRouter } from "next/router";
-import { useCallback, useEffect, useState } from "react";
-import { IToken, Token } from "../../interface/token";
-import {
-  API_ENDPOINT,
-  REFRESH_TOKEN_ENDPOINT,
-  TOKEN_REFRESH_INTERVAL,
-} from "../../utils/constants";
-import {
-  getAccessToken,
-  getRefreshToken,
-  setAccessToken,
-} from "../../utils/helpers";
+import { useEffect } from "react";
+import { TOKEN_REFRESH_INTERVAL } from "../../utils/constants";
+import { refreshAccessToken } from "../../utils/helpers";
 
 const useToken = () => {
-  const [accessToken] = useState<IToken>(new Token(getAccessToken()));
-  const [refreshToken] = useState<IToken>(new Token(getRefreshToken()));
-
   const router = useRouter();
 
-  const refreshAccessToken = useCallback(async () => {
+  useEffect(() => {
     try {
-      const { data } = await axios.post(API_ENDPOINT + REFRESH_TOKEN_ENDPOINT, {
-        refresh: refreshToken,
-      });
-      setAccessToken(data.access_token);
-    } catch (e) {
+      const interval = setInterval(async () => {
+        await refreshAccessToken();
+      }, TOKEN_REFRESH_INTERVAL);
+
+      return () => {
+        clearInterval(interval);
+      };
+    } catch (error) {
       router.push("/login");
     }
-  }, [refreshToken]);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      refreshAccessToken();
-    }, TOKEN_REFRESH_INTERVAL);
-    return () => {
-      clearInterval(interval);
-    };
-  }, [refreshAccessToken]);
-
-  return {
-    access: accessToken.getToken,
-    refresh: refreshToken.getToken,
-    refresh_acess: refreshAccessToken,
-  };
+  });
 };
 
-
-export default useToken
+export default useToken;
