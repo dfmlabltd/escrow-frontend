e2359ef31ad597ac551e87d2983fa94d4b2c7840
iamoracle <38940247+iamoracle@users.noreply.github.com>
2022-10-25 11:45:57 +0100

    🍱 added new assets

--- adehun/utils/constants.ts
+++ adehun/utils/constants.ts
@@ -1,12 +1,12 @@
 export const AUTH_ACCESS_TOKEN: string = "auth_access_token";
 export const AUTH_REFRESH_TOKEN: string = "auth_refresh_token";
-export const TOKEN_REFRESH_INTERVAL: number = 60 * 5 // 5 minutes
+export const TOKEN_REFRESH_INTERVAL: number = 60 * 5; // 5 minutes
 export const CURRENT_USER_EMAIL: string = "current_user_email";
 // endpoints
-export const API_ENDPOINT: string = "http://localhost:8080";
+export const API_ENDPOINT: string = "http://localhost:8000";
 export const LOGIN_WITH_EMAIL_ENDPOINT: string = "/user/email/login/";
 export const LOGIN_WITH_WALLET_ENDPOINT: string = "/signin/otk/";
 export const REFRESH_TOKEN_ENDPOINT: string = "/user/auth/token/refresh/";
 export const ACCESS_TOKEN_ENDPOINT: string = "/user/auth/token/";
 export const USER_PROFILE_ENDPOINT: string = "/user/profile/";
-export const USER_EMAIL_VERIFICATION_ENDPOINT: string = "/user/email"
+export const USER_EMAIL_VERIFICATION_ENDPOINT: string = "/user/email/";
--- adehun/widgets/Headers/Auth.jsx
+++ adehun/widgets/Headers/Auth.jsx
@@ -1,12 +1,12 @@
 const AuthHeader = () => {
   return (
-    <header class="relative" aria-labelledby="Header">
-      <div class="container">
-        <div class="flex justify-center py-4">
+    <header className="relative" aria-labelledby="Header">
+      <div className="container">
+        <div className="flex justify-center py-4">
           <img
             src="../assets/Logo/Group 37470.png"
             alt="Adehun Logo"
-            class="w-16 object-contain"
+            className="w-16 object-contain"
           />
         </div>
       </div>
