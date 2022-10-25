7113c2499e7356be31bd44c72eac7099e5ed2666
iamoracle <38940247+iamoracle@users.noreply.github.com>
2022-10-25 11:48:21 +0100

    🐛 fixed enpoint bug

--- adehun/hooks/authentication/emailLogin.ts
+++ adehun/hooks/authentication/emailLogin.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { useRouter } from "next/router";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { IEmail, Email } from "../../interface/email";
+import customAxios from "../../axios";
 import {
+  ACCESS_TOKEN_ENDPOINT,
   CURRENT_USER_EMAIL,
   LOGIN_WITH_EMAIL_ENDPOINT,
-  USER_EMAIL_VERIFICATION_ENDPOINT,
 } from "../../utils/constants";
 import { setAccessToken, setRefreshToken } from "../../utils/helpers";
 
@@ -14,29 +15,44 @@ export const useEmailLogin = () => {
 
   const [error, setError] = useState<string>("");
 
+  const [loginError, setLoginError] = useState<string>("");
+
   const router = useRouter();
 
   const handleLogin = useCallback(async () => {
     try {
-      await axios.post(LOGIN_WITH_EMAIL_ENDPOINT, {
+      await customAxios.post(LOGIN_WITH_EMAIL_ENDPOINT, {
         email: email.toString(),
       });
       sessionStorage.setItem(CURRENT_USER_EMAIL, email.toString());
+      setLoginError("");
       router.push("/email/verify/");
     } catch (e: any) {
-      setError(e.response.data.details);
+      console.log(e);
+      setLoginError(e);
     }
   }, [email]);
 
-  const handleEmail = (email: string): void => {
-    try {
-      setEmail(new Email(email));
-    } catch (error) {
-      setError("invalid email address");
-    }
-  };
+  const handleEmail = useCallback(
+    (email: string): void => {
+      try {
+        setEmail(new Email(email));
+        setError("");
+      } catch (error) {
+        setError("invalid email address");
+      }
+    },
+    [email]
+  );
 
-  return [email.toString, error, handleEmail, handleLogin];
+  return {
+    email: email.toString(),
+    error,
+    handleEmail,
+    handleLogin,
+    loginError,
+    setLoginError,
+  };
 };
 
 export const useEmailLoginVerify = () => {
@@ -44,24 +60,42 @@ export const useEmailLoginVerify = () => {
 
   const [error, setError] = useState<string>("");
 
+  const [verificationError, setVerificationError] = useState<string>("");
+
   const router = useRouter();
+
+  useEffect(() => {
+    if (code.length > 10) {
+      setError("");
+      return;
+    }
+    setError("invalid code");
+  }, [code]);
 
   const handleVerify = useCallback(async () => {
     try {
-      const getToken = await axios.post(USER_EMAIL_VERIFICATION_ENDPOINT, {
-        code,
+      const getToken = await customAxios.post(ACCESS_TOKEN_ENDPOINT, {
+        password: code,
         email: sessionStorage.getItem(CURRENT_USER_EMAIL),
       });
 
-      const { access_token, refresh_token } = getToken.data;
-      setAccessToken(access_token);
-      setRefreshToken(refresh_token);
+      const { refresh, access } = getToken.data;
+      setAccessToken(access);
+      setRefreshToken(refresh);
+      setVerificationError("");
 
       router.push("/dashboard/");
     } catch (e: any) {
-      setError(e.response.data.details);
+      setVerificationError(e);
     }
   }, [code]);
 
-  return { code, setCode, error, handleVerify };
+  return {
+    code,
+    setCode,
+    error,
+    handleVerify,
+    verificationError,
+    setVerificationError,
+  };
 };
