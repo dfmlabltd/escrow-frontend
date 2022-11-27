aefa7d1f47d435fc3432fe201138ec4966641129
iamoracle <38940247+iamoracle@users.noreply.github.com>
2022-11-27 09:28:40 +0100

    💄 minor ui changes

--- adehun/src/widgets/Dashboard/Card/TransactionCard.jsx
+++ adehun/src/widgets/Dashboard/Card/TransactionCard.jsx
@@ -2,16 +2,16 @@ function Transactioncard() {
   return (
     <div className="grid grid-cols-2 flex-wrap bg-primary p-6 rounded-sm gap-4 justify-between text-white">
       <div className="flex flex-col gap-y-1">
-        <span className="text-sm uppercase">Total Transaction</span>
+        <span className="text-sm uppercase">Total Contracts</span>
         <p className="text-3xl font-bold">20</p>
         <div>
           <span className="bg-black text-xs capitalize text-green-400 p-0.5">
-            21 Transactions in total
+            100 transactions
           </span>
         </div>
       </div>
       <div className="flex flex-col gap-y-1">
-        <span className="text-sm uppercase">Active Transaction</span>
+        <span className="text-sm uppercase">Active Contracts</span>
         <p className="text-3xl font-bold">1</p>
       </div>
     </div>
