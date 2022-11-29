fa262713bf3f043dffcd5aac86eee063226a90f7
iamoracle <38940247+iamoracle@users.noreply.github.com>
2022-11-29 20:12:51 +0100

    🚚 renamed and moved files

---
+++ adehun/src/widgets/Dashboard/Sidebar/index.tsx
@@ -0,0 +1,15 @@
+import SidebarLogo from "./SidebarLogo";
+import SidebarLink from "./Links";
+import FootLinks from "./FootLinks";
+
+const Sidebar = () => {
+  return (
+    <>
+      <SidebarLogo />
+      <SidebarLink />
+      <FootLinks />
+    </>
+  );
+};
+
+export default Sidebar;
