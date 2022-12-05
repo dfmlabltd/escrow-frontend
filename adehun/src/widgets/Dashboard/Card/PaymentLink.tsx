1fe3ba504338b53c8fabc7ac1bb526d41e14ce29
iamoracle <38940247+iamoracle@users.noreply.github.com>
2022-12-05 15:46:50 +0100

    🎉 added support for payment link

--- adehun/src/widgets/Dashboard/Card/PaymentLink.tsx
+++ adehun/src/widgets/Dashboard/Card/PaymentLink.tsx
@@ -1,9 +1,31 @@
+import { useSelector } from "react-redux";
 import useClipboard from "react-use-clipboard";
+import IState from "../../../redux/istore";
 
 const PaymentLink: React.FC = () => {
+  const user: any = useSelector<IState>((state) => state.user);
+
   const [isCopied, setCopied] = useClipboard("https://adehun.com/pay/eax", {
     successDuration: 1000,
   });
+
+  const SetCard = () => {
+    return (
+      <span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-5 h-5"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          style={{ msFilter: "" }}
+          fill="rgba(255, 255, 255, 1)"
+        >
+          <path d="M4 21a1 1 0 00.24 0l4-1a1 1 0 00.47-.26L21 7.41a2 2 0 000-2.82L19.42 3a2 2 0 00-2.83 0L4.3 15.29a1.06 1.06 0 00-.27.47l-1 4A1 1 0 003.76 21 1 1 0 004 21zM18 4.41L19.59 6 18 7.59 16.42 6zM5.91 16.51L15 7.41 16.59 9l-9.1 9.1-2.11.52z"></path>
+        </svg>
+      </span>
+    );
+  };
 
   const CopyCard = () => {
     return isCopied ? (
@@ -59,16 +81,16 @@ const PaymentLink: React.FC = () => {
             </svg>
           </div>
           <span className="text-sm">
-            adehun.com/pay/<span className="font-bold">eax</span>
+            adehun.com/pay/
+            <span className="font-bold">{user.username ?? "{paylink}"}</span>
           </span>
-          <div>
-            <CopyCard />
-          </div>
+          <div>{user.username ? <CopyCard /> : <SetCard />}</div>
         </div>
         <div>
           <p className="text-sm bg-dashbasealt p-3 rounded-sm mt-2">
-            Rather than giving out your email address, why not send your payment
-            link instead?
+            {user.username
+              ? "Rather than giving out your email address, why not send your payment link instead?"
+              : "You are yet to set your payment link."}
           </p>
         </div>
       </div>
