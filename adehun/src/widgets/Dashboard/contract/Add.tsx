5b7f648ca4a95fee5c7a20787668733346670085
iamoracle <38940247+iamoracle@users.noreply.github.com>
2022-12-09 09:11:12 +0100

    🎨 changed js to ts

--- adehun/src/widgets/Dashboard/contract/Add.tsx
+++ adehun/src/widgets/Dashboard/contract/Add.tsx
@@ -1,7 +1,11 @@
-function InvoiceAdd(props) {
+interface Props {
+  addtext: string;
+}
+
+const InvoiceAdd: React.FC<Props> = ({ addtext }: Props) => {
   return (
-    <div class="flex flex-row text-secondary py-2">
-      <div class="flex items-center h-5">
+    <div className="flex flex-row text-secondary py-2">
+      <div className="flex items-center h-5">
         <svg
           className="w-3 h-3"
           xmlns="http://www.w3.org/2000/svg"
@@ -15,13 +19,13 @@ function InvoiceAdd(props) {
           <path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path>
         </svg>
       </div>
-      <div class="ml-2 text-sm">
-        <p id="Addtext" class="text-adbase font-normal uppercase">
-          {props.addtext}
+      <div className="ml-2 text-sm">
+        <p id="Addtext" className="text-adbase font-normal uppercase">
+          {addtext}
         </p>
       </div>
     </div>
   );
-}
+};
 
 export default InvoiceAdd;
