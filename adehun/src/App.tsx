bf6e79c3b89df60f3e57a5d83348a4e2883b18ee
iamoracle <38940247+iamoracle@users.noreply.github.com>
2023-01-16 22:23:49 +0100

    🔨 updated rainbowkit

--- adehun/src/App.tsx
+++ adehun/src/App.tsx
@@ -1,14 +1,15 @@
 import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { mainnet, optimism } from "wagmi/chains";
 
 import PageRoutes from "./routes";
 
 const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [mainnet, optimism],
   [publicProvider()]
 );
 
